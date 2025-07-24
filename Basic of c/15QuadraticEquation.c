5ef10b913efd056d3ce6c21341fc14c69bbc84cf
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-24 08:45:04 +0530

    maths formula solve

---
+++ Basic of c/15QuadraticEquation.c
@@ -0,0 +1,9 @@
+//WAP to calculate of a quadretic equation//
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float a,b,c,d,r1,r2;
+printf("put the values a,b,c");
+scanf("%f%f%f",&a,&b,&c);
+d =sqrt((b*b)-4*a*c);
