2df047c31e70e992eace710f192ed8ad99a9c955
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-26 07:32:14 +0530

    incometax Question

---
+++ Basic of c/19IncomeTax.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    float tax=0,income;
+    printf("enter Your Income\n");
+    scanf("%f",&income);
+    if(income>=250000 && income<=500000)
+    {
+        tax=tax+0.05*(income-250000);
+    }
+    if(income>=500000 && income<=100000)
+    {
+        tax=tax+0.20*(income-500000);
+    }
+    if(income>=1000000)
+    {
+        tax=tax+0.30*(income-1000000);
+
+    }
+   
+
+}
