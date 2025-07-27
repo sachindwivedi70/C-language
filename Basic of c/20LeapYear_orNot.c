cecf9f763f8dc05b4719a8f66b70f98293852dbf
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-27 20:40:08 +0530

    leap year

---
+++ Basic of c/20LeapYear_orNot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("enter the Year\n");
+    scanf("%d",&year);
+    if( year %4==0)
+    {
+        printf("this year is leap year\n");
+    }
+    else
+   
