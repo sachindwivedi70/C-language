73f1843c82cdd75217429087785bd1a9b331e03c
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-28 09:39:20 +0530

    rating Question

---
+++ Basic of c/23Rating.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int rating;
+    printf("enter your rating(1-5)\n");
+    scanf("%d",&rating);
+    switch (rating)
+    {
+    case 1 : 
+    printf("your rating is 1\n ");
+break;
+    case 2 :
+    printf("your rating is 2\n");
+    break;
+     case 3 : 
+    printf("your rating is 3\n ");
+
+    
+    
+}
