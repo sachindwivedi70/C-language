b3864a685cd956458cad7a837ff09072510b2d23
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-28 09:39:44 +0530

    done

--- Basic of c/23Rating.c
+++ Basic of c/23Rating.c
@@ -14,7 +14,18 @@ break;
     break;
      case 3 : 
     printf("your rating is 3\n ");
-
+break;
+ case 4 : 
+    printf("your rating is 4\n ");
+break;
+ case 5 : 
+    printf("your rating is 5\n ");
+break;
+default:
+printf("invalid rating\n");
+break;
+return 0;
+    }
     
     
 }
