23963053e7d1558c0aa07a83ee3ae0c0164d9b3f
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-30 09:59:05 +0530

    One more solve

---
+++ Loop/32_Natural_no_RevesOrder.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter any no:");
+    scanf("%d",&n);
+    for(int i=n;i;i--){
+        printf("natural numbers:%d\n",i);
+    }return 0;
+
+}
