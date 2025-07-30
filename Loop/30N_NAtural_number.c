d75c3db5fbae9ec4a65a781bc37abca187e955ea
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-30 09:57:33 +0530

    natural number type question

---
+++ Loop/30N_NAtural_number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a,i=0;
+    printf("enter any value");
+    scanf("%d",&a);
+    for(int i=0;i<=a;i++){
+        printf("the natural number is %d\n",i);
+    }return 0;
+}
