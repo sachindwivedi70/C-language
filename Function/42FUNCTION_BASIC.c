a2ad000059eaa94f0a83a30bf2e39165fdb6f04c
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-07-31 09:36:32 +0530

    Function

---
+++ Function/42FUNCTION_BASIC.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void goodmorning();
+void goodafternoon();
+void goodnight();
+int main (){
+goodmorning();
+goodafternoon();
+goodnight();
+return 0;
+}
+void goodmorning(){
+    printf("good morning sachinm\n");
+}
+void goodafternoon(){
+printf("good afternoon sachin\n");
+
+}
+void goodnight(){
+    printf("good night sachin\n");
+    }
