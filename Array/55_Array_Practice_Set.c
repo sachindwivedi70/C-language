71a32eddeb2837022b3ad29128343139e1a5108e
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-08-01 16:05:59 +0530

    location in memeory

---
+++ Array/55_Array_Practice_Set.c
@@ -0,0 +1,14 @@
+f#include<stdio.h>
+int main (){
+    int arr[10];
+    int *ptr=arr;
+    ptr=ptr+2;
+    if(ptr==&arr[2]){
+        printf("these point to the same location in memory"); 
+    }
+    else{
+         printf("these do not point to the same location in memory"); 
+    }
+    return 0;
+
+}
