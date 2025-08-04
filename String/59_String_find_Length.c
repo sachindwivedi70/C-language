57043d0236d9538a03e62cb4aaa68e8aed688f31
sachindwivedi70 <sachindwivedi7084@gmail.com>
2025-08-04 12:56:29 +0530

    Length Find

---
+++ String/59_String_find_Length.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int countlength(char arr[]);
+int main (){
+char name[100];
+fgets(name,100,stdin);
+printf("length is  %d",countlength(name));
+    return 0;
+}
+int countlength(char arr[])
+{
+    int count =0;
+    for(int i=0;arr[i]!='\0';i++){
+        count++;
+
+    }return count-1;
+}
